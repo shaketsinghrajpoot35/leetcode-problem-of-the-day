c4840c69c91550f3ea4ad743977273cd72c72d19
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-03-02 14:55:22 +0530

    Time: 14 ms (97.06%) | Memory: 98 MB (66.57%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,3 +1,37 @@
+// class Solution {
+
+//     public void backtracking(int start, int n, int k, 
+//                              List<List<Integer>> combinations, 
+//                              List<Integer> list) {
+
+//         // Base Case
+//         if (list.size() == k) {
+//             combinations.add(new ArrayList<>(list));
+//             return;
+//         }
+
+//         // Loop from start to n
+//         for (int i = start; i <= n; i++) {
+
+//             list.add(i);  // choose
+
+//             backtracking(i + 1, n, k, combinations, list);  // explore
+
+//             list.remove(list.size() - 1);  // un-choose (backtrack)
+//         }
+//     }
+
+//     public List<List<Integer>> combine(int n, int k) {
+
+//         List<List<Integer>> combinations = new ArrayList<>();
+
+//         backtracking(1, n, k, combinations, new ArrayList<>());
+
+//         return combinations;
+//     }
+// }
+
+
 class Solution {
 
     public void backtracking(int start, int n, int k, 
@@ -11,11 +45,12 @@ class Solution {
         }
 
         // Loop from start to n
-        for (int i = start; i <= n; i++) {
+        int remaining = k - list.size();
+        for (int i = start; i <= n-remaining+1; i++) {
 
             list.add(i);  // choose
 
-            backtracking(i + 1, n, k, combinations, list);  // explore
+            backtracking( i+ 1, n, k, combinations, list);  // explore
 
             list.remove(list.size() - 1);  // un-choose (backtrack)
         }
