e25953a0e80b97b37bd0def7f58a4fa30f63bfa4
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-03-06 20:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (54.59%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,39 +1,40 @@
-// class Solution {
-//     public void reverseString(char[] s) {
-//         int i=0;
-//         int j= s.length-1;
-//         while(i<=j){
-//             char temp = s[i];
-//             s[i] = s[j];
-//             s[j] = temp;
-//             i++;
-//             j--;
-//         }
-//     }
-// }
-
 class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j= s.length-1;
+        while(i<=j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
+
+// class Solution {
 
    
 
-    public void recurstion(char[] s,int i, int j){
+//     public void recurstion(char[] s,int i, int j){
 
-        if(i>j){
-            return;
-        }
+//         if(i>=j){
+//             return;
+//         }
 
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
+//         char temp = s[i];
+//         s[i] = s[j];
+//         s[j] = temp;
 
-        recurstion(s,i+1,j-1);
+//         recurstion(s,i+1,j-1);
         
 
 
-    }
-    public void reverseString(char[] s) {
+//     }
+//     public void reverseString(char[] s) {
 
        
-        recurstion(s,0,s.length-1);
-    }
-}
+//         recurstion(s,0,s.length-1);
+//     }
+// }
+
