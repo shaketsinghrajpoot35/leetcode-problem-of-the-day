008566c4f5dac616083f1a5135ed8e3ca3b8cb2c
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-03-07 19:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (82.52%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -8,57 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-
-//     public void recursion(ListNode head){
-
-//         if(head == null || head.next == null){
-//             return;
-//         }
-
-//         recursion(head.next);
-
-
-//         //swap valus
-//         int temp = head.val;
-//         head.val = head.next.val;
-//         head.next.val = temp;
-
-//     }
-//     public ListNode swapPairs(ListNode head) {
-        
-//        recursion( head);
-
-//        return head;
-//     }
-// }
 
 
 class Solution {
 
-   
-    public ListNode swapPairs(ListNode head) {
+    public void recursion(ListNode fistNode, ListNode secondNode){
 
+        if(fistNode == null || secondNode == null){
+            return;
+        }
+
+        int temp = fistNode.val;
+        fistNode.val = secondNode.val;
+        secondNode.val = temp;
+
+        //now next pairs
+
+        fistNode = secondNode.next;
+        if(fistNode == null) return;
+        secondNode = fistNode.next;
+
+        recursion(fistNode,secondNode);
+
+    }
+    public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null){
             return head;
         }
-        ListNode temp1 = head;
-        ListNode temp2 = head.next;
+        recursion(head,head.next);
 
-        while(temp1 != null && temp2 != null){
-
-            int temp = temp1.val;
-            temp1.val = temp2.val;
-            temp2.val = temp;
-
-            temp1 = temp2.next;
-            if(temp1 == null){
-                return head;
-            }
-            temp2 = temp1.next;
-
-        }
-
-        return head;
+       return head;
     }
 }
+
+
+// class Solution {
+
+   
+//     public ListNode swapPairs(ListNode head) {
+
+//         if(head == null || head.next == null){
+//             return head;
+//         }
+//         ListNode temp1 = head;
+//         ListNode temp2 = head.next;
+
+//         while(temp1 != null && temp2 != null){
+
+//             int temp = temp1.val;
+//             temp1.val = temp2.val;
+//             temp2.val = temp;
+
+//             temp1 = temp2.next;
+//             if(temp1 == null){
+//                 return head;
+//             }
+//             temp2 = temp1.next;
+
+//         }
+
+//         return head;
+//     }
+// }
