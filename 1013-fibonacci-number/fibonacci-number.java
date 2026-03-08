cda8e40b10af67110e7103b7d9584bbca311dbfc
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-03-08 18:03:03 +0530

    Time: 30 ms (9.59%) | Memory: 42.2 MB (49.67%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,37 @@
-class Solution {
-    public int fib(int n) {
+// class Solution {
+//     public int fib(int n) {
         
-        if(n==0 || n==1) return n;
+//         if(n==0 || n==1) return n;
 
-        return fib(n-1) + fib(n-2);
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+
+
+class Solution {
+
+    HashMap<Integer,Integer> map;
+
+    public int fibHelper(int n){
+
+        //base case
+        if(n == 0 || n==1){
+            return n;
+        }
+
+        //already calls executed so result store in map
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+
+        return fibHelper(n-1) + fibHelper(n-2);
+
+    }
+    public int fib(int n) {
+
+        map = new HashMap<Integer,Integer>();
+
+        return fibHelper(n);
+        
     }
 }
