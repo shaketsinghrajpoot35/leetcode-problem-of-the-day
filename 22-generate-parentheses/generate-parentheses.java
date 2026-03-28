d2f0c840f0ef8e4f761a093a3446ba1fe6bcc2e9
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-03-28 19:57:04 +0530

    Time: 23 ms (5.06%) | Memory: 54.8 MB (5.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,3 +1,53 @@
+// class Solution {
+
+//     public boolean isValidParenthesis(String s){
+
+//         int c = 0;
+//         for(char ch : s.toCharArray()){
+
+//             if(ch =='('){
+//                 c++;
+//             }
+
+//             else if(ch == ')'){
+//                 c--;
+//             }
+
+//             if(c<0){
+//                 return false;
+//             }
+//         }
+
+//         return c==0;
+//     }
+
+//     public void generateParenthesisHelper(int n, String s, List<String> ans){
+
+//         if(s.length()==2*n){
+
+//             if(isValidParenthesis(s)){
+//                 ans.add(s);
+//             }
+
+//             return;
+//         }
+
+//         generateParenthesisHelper(n, s+"(", ans);
+//         generateParenthesisHelper(n, s+")", ans);
+
+
+//     }
+//     public List<String> generateParenthesis(int n) {
+        
+//         List<String> ans = new ArrayList<>();
+//         generateParenthesisHelper(n,"", ans);
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
 
     public boolean isValidParenthesis(String s){
@@ -21,26 +71,28 @@ class Solution {
         return c==0;
     }
 
-    public void generateParenthesisHelper(int n, String s, List<String> ans){
-
-        if(s.length()==2*n){
-
-            if(isValidParenthesis(s)){
-                ans.add(s);
-            }
-
-            return;
-        }
-
-        generateParenthesisHelper(n, s+"(", ans);
-        generateParenthesisHelper(n, s+")", ans);
-
-
-    }
     public List<String> generateParenthesis(int n) {
         
         List<String> ans = new ArrayList<>();
-        generateParenthesisHelper(n,"", ans);
+        
+        Queue<String> q = new LinkedList<>();
+        q.add("");
+
+        while(!q.isEmpty()){
+
+            String curr = q.remove();
+            if(curr.length() == 2*n){
+
+                if(isValidParenthesis(curr)){
+                    ans.add(curr);
+                }
+
+                continue;
+            }
+
+            q.add(curr+"(");
+            q.add(curr+")");
+        }
 
         return ans;
     }
