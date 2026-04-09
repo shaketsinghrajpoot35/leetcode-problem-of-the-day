0bf4c4bf89e60d5206758836ab9a8b4472f2ce26
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-04-09 19:40:22 +0530

    Time: 32 ms (9.90%) | Memory: 54 MB (40.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,18 @@
+
 class Solution {
 
-     HashMap<String, Integer> map;
+    int t[][] ;
 
-     int longestCommonSubsequenceHelper(String text1, String text2, int m, int n) {
+    int longestCommonSubsequenceHelper(String text1, String text2, int m, int n) {
 
         if (m == 0 || n == 0) {
             return 0;
         }
 
-        String key = m + "," + n;
+      
 
-        if (map.containsKey(key)) {
-            return map.get(key);
+        if (t[m][n] != -1) {
+            return t[m][n];
         }
 
         int result;
@@ -24,15 +25,65 @@ class Solution {
             result = Math.max(option1, option2);
         }
 
-        map.put(key, result);
+        t[m][n] = result ;
         return result;
     }
 
    public int longestCommonSubsequence(String s1, String s2) {
-        map = new HashMap<>();
-        return longestCommonSubsequenceHelper(s1, s2, s1.length(), s2.length());
+        int m = s1.length();
+        int n = s2.length();
+        t = new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            
+            for(int j=0;j<n+1;j++){
+                
+                t[i][j] = -1;
+            }
+        }
+        return longestCommonSubsequenceHelper(s1, s2, m, n);
     }
 }
+
+
+
+
+
+
+// class Solution {
+
+//      HashMap<String, Integer> map;
+
+//      int longestCommonSubsequenceHelper(String text1, String text2, int m, int n) {
+
+//         if (m == 0 || n == 0) {
+//             return 0;
+//         }
+
+//         String key = m + "," + n;
+
+//         if (map.containsKey(key)) {
+//             return map.get(key);
+//         }
+
+//         int result;
+
+//         if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+//             result = 1 + longestCommonSubsequenceHelper(text1, text2, m - 1, n - 1);
+//         } else {
+//             int option1 = longestCommonSubsequenceHelper(text1, text2, m, n - 1);
+//             int option2 = longestCommonSubsequenceHelper(text1, text2, m - 1, n);
+//             result = Math.max(option1, option2);
+//         }
+
+//         map.put(key, result);
+//         return result;
+//     }
+
+//    public int longestCommonSubsequence(String s1, String s2) {
+//         map = new HashMap<>();
+//         return longestCommonSubsequenceHelper(s1, s2, s1.length(), s2.length());
+//     }
+// }
 
 
 
