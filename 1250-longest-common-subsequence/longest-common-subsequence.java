63084af7ababc82f67f19f9a51f8ea387d20175c
Shaket Singh Rajpoot <rajpootsaket35@gmail.com>
2026-04-09 19:53:30 +0530

    Time: 20 ms (78.09%) | Memory: 53.7 MB (81.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,48 +1,83 @@
 
 class Solution {
 
-    int t[][] ;
-
-    int longestCommonSubsequenceHelper(String text1, String text2, int m, int n) {
-
-        if (m == 0 || n == 0) {
-            return 0;
-        }
-
-      
-
-        if (t[m][n] != -1) {
-            return t[m][n];
-        }
-
-        int result;
-
-        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
-            result = 1 + longestCommonSubsequenceHelper(text1, text2, m - 1, n - 1);
-        } else {
-            int option1 = longestCommonSubsequenceHelper(text1, text2, m, n - 1);
-            int option2 = longestCommonSubsequenceHelper(text1, text2, m - 1, n);
-            result = Math.max(option1, option2);
-        }
-
-        t[m][n] = result ;
-        return result;
-    }
 
    public int longestCommonSubsequence(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
-        t = new int[m+1][n+1];
+        int [][]t = new int[m+1][n+1];
         for(int i=0;i<m+1;i++){
             
             for(int j=0;j<n+1;j++){
                 
-                t[i][j] = -1;
+               if(i==0 || j==0){
+                  t[i][j] = 0;
+               }
             }
         }
-        return longestCommonSubsequenceHelper(s1, s2, m, n);
+        
+        for(int i=1;i<m+1;i++){
+            
+            for(int j=1;j<n+1;j++){
+                
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                     t[i][j]  = 1 + t[i-1][j-1];
+                } else{
+
+                    t[i][j] = Math.max(t[i-1][j] , t[i][j-1]);
+                }
+            }
+        }
+
+        return t[m][n];
     }
 }
+
+
+// class Solution {
+
+//     int t[][] ;
+
+//     int longestCommonSubsequenceHelper(String text1, String text2, int m, int n) {
+
+//         if (m == 0 || n == 0) {
+//             return 0;
+//         }
+
+      
+
+//         if (t[m][n] != -1) {
+//             return t[m][n];
+//         }
+
+//         int result;
+
+//         if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+//             result = 1 + longestCommonSubsequenceHelper(text1, text2, m - 1, n - 1);
+//         } else {
+//             int option1 = longestCommonSubsequenceHelper(text1, text2, m, n - 1);
+//             int option2 = longestCommonSubsequenceHelper(text1, text2, m - 1, n);
+//             result = Math.max(option1, option2);
+//         }
+
+//         t[m][n] = result ;
+//         return result;
+//     }
+
+//    public int longestCommonSubsequence(String s1, String s2) {
+//         int m = s1.length();
+//         int n = s2.length();
+//         t = new int[m+1][n+1];
+//         for(int i=0;i<m+1;i++){
+            
+//             for(int j=0;j<n+1;j++){
+                
+//                 t[i][j] = -1;
+//             }
+//         }
+//         return longestCommonSubsequenceHelper(s1, s2, m, n);
+//     }
+// }
 
 
 
